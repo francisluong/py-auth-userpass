1cfe8380ab7ee9c6ee3c0280348b91e53c60e253
Francis Luong (Franco) <franco@definefunk.com>
2014-06-30 17:14:20 -0400

    pypi packaging mods

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author = "Francis Luong (Franco)",
     author_email = "networkascode@definefunk.com",
     description = ("Simple but not so secure username and password handling"),
-    license = "MIT",
+    license = "LICENSE.txt",
     url = "https://github.com/francisluong/py-auth-userpass",
     install_requires=install_reqs,
     packages=['auth']
